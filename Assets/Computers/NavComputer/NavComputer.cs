c1891b2037b304732419dd80de50298a2780d70f
Eklofakka <Lekarlsson200@gmail.com>
2018-08-04 15:20:29 +0200

    NavComputer

--- Assets/Computers/NavComputer/NavComputer.cs
+++ Assets/Computers/NavComputer/NavComputer.cs
@@ -8,11 +8,48 @@ namespace Computers
     {
         public class NavComputer : MonoBehaviour
         {
-            public Transform MainShipIcon;
+            private Transform MainShipIcon;
+
+            [SerializeField] private GameObject SpacePlane;
+
+            [Header("Prefabs")]
+            [SerializeField] private GameObject MainShipIconPrefab;
+            [SerializeField] private GameObject StarSystemPrefab;
 
             private void Start()
             {
-                MainShipIcon.localPosition = Ship.MainShip.Position.ToV3();
+                SpawnMap();
+            }
+
+            private void SpawnMap()
+            {
+                SpawnMainShip();
+
+                SpawnStars();
+            }
+
+            private void SpawnMainShip()
+            {
+                GameObject mainShip = Instantiate( MainShipIconPrefab );
+                mainShip.transform.SetParent( SpacePlane.transform, false );
+                mainShip.transform.localPosition = Ship.MainShip.Position.ToV3();
+
+                MainShipIcon = mainShip.transform;
+            }
+
+            private void SpawnStars()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    SpawnStar( new SolarSystem() );
+                }
+            }
+
+            private void SpawnStar( SolarSystem solarSystem )
+            {
+                GameObject obj = Instantiate( StarSystemPrefab );
+                obj.transform.SetParent( SpacePlane.transform, false );
+                obj.transform.localPosition = solarSystem.Position.ToV3();
             }
         }
     }
