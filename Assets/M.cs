17b1241a827e32dde8ca7dcdd8e8b84b7ec4563c
Eklofakka <Lekarlsson200@gmail.com>
2018-08-04 12:31:26 +0200

    NavComputer

---
+++ Assets/M.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class M : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    public Transform p;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        p.transform.localPosition += new Vector3( eventData.delta.x/2, eventData.delta.y/2, 0 );
+
+        p.transform.localPosition = p.transform.localPosition.ToNearestWhole();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //transform.position = Input.mousePosition;
+    }
+}
---
+++ Assets/System/VectorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorExtensions
+{
+    public static Vector3 RoundToInt ( this Vector3 v3 )
+    {
+        v3.x = Mathf.RoundToInt( v3.x );
+        v3.y = Mathf.RoundToInt( v3.y );
+        v3.z = Mathf.RoundToInt( v3.z );
+
+        return v3;
+    }
+
+    public static Vector3 ToNearestWhole( this Vector3 v3 )
+    {
+        v3 = v3.RoundToInt();
+
+        v3.x = v3.x % 2 == 0 ? v3.x : v3.x + 1;
+        v3.y = v3.y % 2 == 0 ? v3.y : v3.y + 1;
+        v3.z = v3.z % 2 == 0 ? v3.z : v3.z + 1;
+
+        return v3;
+    }
+}
