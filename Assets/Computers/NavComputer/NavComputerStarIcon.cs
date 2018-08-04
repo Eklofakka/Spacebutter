19501e0ce693c572dc00f216225514d58b933dff
Eklofakka <Lekarlsson200@gmail.com>
2018-08-04 16:51:03 +0200

    NavComputer

--- Assets/Computers/NavComputer/NavComputerStarIcon.cs
+++ Assets/Computers/NavComputer/NavComputerStarIcon.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class NavComputerStarIcon : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 {
     public SolarSystem SolarSystem;
+
+    [SerializeField] private Image Selector;
 
     public void OnPointerClick(PointerEventData eventData)
     {
