928a87c1185144df03aee50362081dab264551f1
Eklofakka <Lekarlsson200@gmail.com>
2018-08-08 19:44:50 +0200

    PwrComputer

---
+++ Assets/Computers/PwrComputer/IPowerGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerGeneratorBase
+{
+    public virtual float Power()
+    {
+        return 5f;
+    }
+
+}
---
+++ Assets/Computers/PwrComputer/PwrComputer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Computers
+{
+    namespace Power
+    {
+        public class PwrComputer : MonoBehaviour
+        {
+            public static PwrComputer Inst;
+
+            private static List<PowerGeneratorBase> Generators = new List<PowerGeneratorBase>();
+
+            private static float Power = 0;
+
+            [SerializeField] private TextMeshProUGUI PowerDisplay;
+
+            private void Awake()
+            {
+                if (Inst != null) Debug.LogError( "More than one PwrComputer" );
+                Inst = this;
+            }
+
+            private void Start()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Generators.Add( new PowerGeneratorBase( 0.1f ) );
+                }
+            }
+
+            private void Update()
+            {
+                CalculatePower();
+
+                PowerDisplay.text = Power.ToString();
+            }
+
+            private float CalculatePower()
+            {
+                float newVal = ( IncPower() - OutPower() ) * Time.deltaTime;
+
+                Power += newVal;
+
+                return Power;
+            }
+
+            private float OutPower()
+            {
+                float pwr = 0f;
+
+                return pwr;
+            }
+
+            private float IncPower()
+            {
+                float pwr = 0f;
+
+                for (int i = 0; i < Generators.Count; i++)
+                {
+                    pwr += Generators[i].Value;
+                }
+
+                return pwr;
+            }
+        }
+    }
+}
