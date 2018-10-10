18a7e8dc9eca101db480be5801afbf2fa6b33af3
Eklofakka <Lekarlsson200@gmail.com>
2018-10-10 19:52:16 +0200

    Dev

---
+++ Assets/TestArea/d.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class d : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var f = gameObject.AddComponent<PolygonCollider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
